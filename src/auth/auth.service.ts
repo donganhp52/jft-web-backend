e5035971cc98ffc179b96b0e9ee0e4bb8174445a
quanlv.56 <quanlv.56@gmail.com>
2026-01-24 15:12:43 +0700

    feat(auth): implement user registration with validation and password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async register(registerDto: RegisterDto) {
+    const { email, password } = registerDto;
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new Error('User with this email already exists');
+    }
+
+    // 2.Hash password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // 3.Tạo user
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        password: passwordHash,
+      },
+      select: {
+        id: true,
+        email: true,
+        role: true,
+        createdAt: true,
+      },
+    });
+    return user;
+  }
 }
